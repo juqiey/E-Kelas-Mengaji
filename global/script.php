c6820568540f9a59b5dd232a181cf53e1e150da9
Wan Marzuqi Amrin Bin Wan Mansor <marzuqi290601@gmail.com>
2023-12-24 11:30:00 +0800

    push script.php & calendar plugin

---
+++ global/script.php
@@ -0,0 +1,12 @@
+<!-- Template plugin here -->
+
+
+<!-- Calendar JavaScript -->
+<script src='../lib/calendar/lib/main.js'></script>
+
+<!-- Script datatable -->
+<script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+<link href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.8/countUp.min.js"></script>
