c7d1d526775dce2c4584578a54cd5acfb27b23da
sofea <sofea.mohdazman@gmail.com>
2024-01-27 20:04:46 +0800

    update

--- model/profile_function.php
+++ model/profile_function.php
@@ -16,15 +16,77 @@ function getStudentProfile($student_id){
     }
 }
 
+// function updateStudentProfile($studentid, $studentname, $studentemail, $hashedPassword) {
+//     global $conn;
+//     $sql = "UPDATE student SET studentname = ?, studentemail = ?, studentpassword = ? WHERE studentid = ?";
+//     $stmt = $conn->prepare($sql);
+//     $stmt->bind_param("ssssi", $studentname, $studentemail, $hashedPassword, $studentid);
+//     $stmt->execute();
+//     $stmt->close();
+//  }
 
-function updateStudentProfile($student_id, $name, $class, $img_ucua_url, $img_action_url) {
+function updateStudentProfile($studentid, $studentname, $studentbirth, $studentgender, $studentnum, $studentemail, $studentaddress, $studentposkod, $studentcity, $parentsname, $parentsnum, $newPassword) {
     global $conn;
-    $sql = "UPDATE student SET studentname = ?, studentclass = ?, img_ucua = ?, img_action = ? WHERE studentid = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param('ssssi', $name, $class, $img_ucua_url, $img_action_url, $student_id);
-    $result = $stmt->execute();
+
+    // Update the student's name
+    $stmt = $conn->prepare("UPDATE student SET studentname = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentname, $studentid);
+    $stmt->execute();
+
+    // Update the student's date of birth
+    $stmt = $conn->prepare("UPDATE student SET studentbirth = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentbirth, $studentid);
+    $stmt->execute();
+
+    // Update the student's gender
+    $stmt = $conn->prepare("UPDATE student SET studentgender = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentgender, $studentid);
+    $stmt->execute();
+
+    // Update the student's phone number
+    $stmt = $conn->prepare("UPDATE student SET studentnum = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentnum, $studentid);
+    $stmt->execute();
+
+    // Update the student's email address
+    $stmt = $conn->prepare("UPDATE student SET studentemail = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentemail, $studentid);
+    $stmt->execute();
+
+    // Update the student's address
+    $stmt = $conn->prepare("UPDATE student SET studentaddress = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentaddress, $studentid);
+    $stmt->execute();
+
+    // Update the student's postal code
+    $stmt = $conn->prepare("UPDATE student SET studentposkod = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentposkod, $studentid);
+    $stmt->execute();
+
+    // Update the student's city
+    $stmt = $conn->prepare("UPDATE student SET studentcity = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $studentcity, $studentid);
+    $stmt->execute();
+
+    // Update the student's parent or guardian's name
+    $stmt = $conn->prepare("UPDATE student SET parentsname = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $parentsname, $studentid);
+    $stmt->execute();
+
+    // Update the student's parent or guardian's phone number
+    $stmt = $conn->prepare("UPDATE student SET parentsnum = ? WHERE studentid = ?");
+    $stmt->bind_param("si", $parentsnum, $studentid);
+    $stmt->execute();
+
+    // Update the student's password
+    if (!empty($newPassword)) {
+        $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+        $stmt = $conn->prepare("UPDATE student SET studentpassword = ? WHERE studentid = ?");
+        $stmt->bind_param("si", $hashedPassword, $studentid);
+        $stmt->execute();
+    }
+
     $stmt->close();
-    return $result;
 }
 
 function getTeacherProfile($teacher_id) {
--- view/edit_student_profile.php
+++ view/edit_student_profile.php
@@ -1,90 +1,93 @@
 <?php
-require '../model/profile_function.php';
+  require '../model/profile_function.php';
 
-$student_id = 2;
-$student_data = getStudentProfile($student_id);
+  $student_id = 2;
+  $student_data = getStudentProfile($student_id);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?
-$title = "Edit Profile Pelajar";
-require '../global/header.php';
+<head>
+  <?php
+  $title = "Edit Profile Pelajar";
+  require '../global/header.php';
+  ?>
+  <link href="../resources/main.css" rel="stylesheet" />
+  <script src="../js/updateStudentProfile.js"></script>
+  <form id="updateStudentForm" method="post" action="update_student_profile.php">
 
-?>
-<link href="../resources/main.css" rel="stylesheet" />
-<style>
-  .account-settings .user-profile {
-    margin: 0 0 1rem 0;
-    padding-bottom: 1rem;
-    text-align: center;
-  }
+  <style>
+    .account-settings .user-profile {
+      margin: 0 0 1rem 0;
+      padding-bottom: 1rem;
+      text-align: center;
+    }
 
-  .profile_img {
-    width: 150px;
-    height: 150px;
-    object-fit: cover;
-    margin: 10px auto;
-    border: 10px solid #ccc;
-    border-radius: 50%;
-  }
+    .profile_img {
+      width: 150px;
+      height: 150px;
+      object-fit: cover;
+      margin: 10px auto;
+      border: 10px solid #ccc;
+      border-radius: 50%;
+    }
 
-  .account-settings .user-profile .user-avatar {
-    margin: 0 0 1rem 0;
-  }
+    .account-settings .user-profile .user-avatar {
+      margin: 0 0 1rem 0;
+    }
 
-  .account-settings .user-profile .user-avatar img {
-    width: 90px;
-    height: 90px;
-    -webkit-border-radius: 100px;
-    -moz-border-radius: 100px;
-    border-radius: 100px;
-  }
+    .account-settings .user-profile .user-avatar img {
+      width: 90px;
+      height: 90px;
+      -webkit-border-radius: 100px;
+      -moz-border-radius: 100px;
+      border-radius: 100px;
+    }
 
-  .account-settings .user-profile h5.user-name {
-    margin: 0 0 0.5rem 0;
-  }
+    .account-settings .user-profile h5.user-name {
+      margin: 0 0 0.5rem 0;
+    }
 
-  .account-settings .user-profile h6.user-email {
-    margin: 0;
-    font-size: 0.8rem;
-    font-weight: 400;
-    color: #9fa8b9;
-  }
+    .account-settings .user-profile h6.user-studentemail {
+      margin: 0;
+      font-size: 0.8rem;
+      font-weight: 400;
+      color: #9fa8b9;
+    }
 
-  .account-settings .about {
-    margin: 2rem 0 0 0;
-    text-align: center;
-  }
+    .account-settings .about {
+      margin: 2rem 0 0 0;
+      text-align: center;
+    }
 
-  .account-settings .about h5 {
-    margin: 0 0 15px 0;
-    color: #007ae1;
-  }
+    .account-settings .about h5 {
+      margin: 0 0 15px 0;
+      color: #007ae1;
+    }
 
-  .account-settings .about p {
-    font-size: 0.825rem;
-  }
+    .account-settings .about p {
+      font-size: 0.825rem;
+    }
 
-  .form-control {
-    border: 1px solid #cfd1d8;
-    -webkit-border-radius: 2px;
-    -moz-border-radius: 2px;
-    border-radius: 2px;
-    font-size: .825rem;
-    background: #ffffff;
-    color: #2e323c;
-  }
+    .form-control {
+      border: 1px solid #cfd1d8;
+      -webkit-border-radius: 2px;
+      -moz-border-radius: 2px;
+      border-radius: 2px;
+      font-size: .825rem;
+      background: #ffffff;
+      color: #2e323c;
+    }
 
-  .card {
-    background: #ffffff;
-    -webkit-border-radius: 5px;
-    -moz-border-radius: 5px;
-    border-radius: 5px;
-    border: 0;
-    margin-bottom: 1rem;
-  }
-</style>
+    .card {
+      background: #ffffff;
+      -webkit-border-radius: 5px;
+      -moz-border-radius: 5px;
+      border-radius: 5px;
+      border: 0;
+      margin-bottom: 1rem;
+    }
+  </style>
 </head>
 
 <body class="sb-nav-fixed">
@@ -144,8 +147,8 @@ require '../global/header.php';
                         </div>
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                           <div class="form-group">
-                            <label for="fullName">Nama Penuh</label>
-                            <input type="text" class="form-control" id="fullName"
+                            <label for="studentname">Nama Penuh</label>
+                            <input type="text" class="form-control" id="studentname"
                               value="<?php echo $student_data['studentname']; ?>">
                           </div>
                         </div>
@@ -172,9 +175,9 @@ require '../global/header.php';
                         </div>
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                           <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email"
-                              value="<?php echo $student_data['studentemail']; ?>">
+                            <label for="studentemail">studentemail</label>
+                            <input type="studentemail" class="form-control" id="studentemail"
+                              value="<?php echo $student_data['studentstudentemail']; ?>">
                           </div>
                         </div>
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
@@ -205,8 +208,8 @@ require '../global/header.php';
                           </div>
                           <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                             <div class="form-group">
-                              <label for="fullName">Nama Penuh</label>
-                              <input type="text" class="form-control" id="fullName" placeholder=""
+                              <label for="studentname">Nama Penuh</label>
+                              <input type="text" class="form-control" id="studentname" placeholder=""
                                 value="<?php echo $student_data['parentsname'] ?>">
                             </div>
                           </div>
