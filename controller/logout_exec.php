7d76871e271374c8efa431d4895bb0ff92c1a7d8
sofea <sofea.mohdazman@gmail.com>
2024-01-30 10:37:58 +0800

    update logout

--- controller/logout_exec.php
+++ controller/logout_exec.php
@@ -1,13 +1,7 @@
 <?php 
 session_start();
-unset($_SESSION["auth"]);
-unset($_SESSION["id"]);
-unset($_SESSION["username"]);
-unset($_SESSION["name"]);
-unset($_SESSION["ic"]);
-unset($_SESSION["phone_no"]);
-unset($_SESSION["email"]);
-unset($_SESSION["company_id"]);
+unset($_SESSION['email']);  
+unset($_SESSION["password"]);
 session_destroy();
 header("Location:../view/login.php");
 ?>
